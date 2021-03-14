07435bab11bee8c611809c1c128426cbaa87fcc0
Stefan Eicher <stefan.eicher@gmail.com>
2021-03-15 00:52:33 +0100

    add gcc-arm-none-eabi-9

--- build.py
+++ build.py
@@ -37,5 +37,9 @@ if __name__ == '__main__':
     run(f'conan export stm32_cmsis_core')
     run(f'conan export stm32_cmsis_device_l4')
     run(f'conan export stm32_hal_l4')
+
+
     run(f'conan create stm32_cmsis_core --build=missing')
     run(f'conan create stm32_hal_l4 --build=missing')
+    run(f"conan create stm32_cmsis_device_wb  -pr=./.profiles/gcc-arm-none-eabi-9")
+
