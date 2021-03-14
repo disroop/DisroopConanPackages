d345cb9803fe09ebb31fe3e3acfbfee68d01cb0e
Stefan Eicher <stefan.eicher@gmail.com>
2021-03-15 00:49:02 +0100

    add stm32_cmsis_device_wb

---
+++ stm32_cmsis_device_wb/conanfile.py
@@ -0,0 +1,45 @@
+import os
+
+from conans import ConanFile, CMake, tools
+
+device_version = "1.8.0"
+project_version = os.getenv("PROJECT_VERSION", "snapshot")
+project_username = os.getenv("CONAN_USERNAME", "disroop")
+project_channel = os.getenv("CONAN_CHANNEL", "development")
+
+
+class Stm32CmsisDeviceL4(ConanFile):
+    name = "stm32_cmsis_device_wb"
+    url = "todo add url"
+    version = f"{device_version}"
+    channel = f"{project_channel}"
+    user = f"{project_username}"
+    license = "MIT"
+    description = "This is the runtime setup of CMSIS devices provided by stm"
+    generators = "virtualenv", "cmake"
+    settings = "os", "compiler", "build_type", "arch"
+    exports_sources = "CMakeLists.txt"
+
+    def requirements(self):
+        self.requires(f"stm32_cmsis_core/5.6.0_cm4@{project_username}/{project_channel}")
+
+    def source(self):
+        git = tools.Git(folder=self.source_folder)
+        git.clone("https://github.com/STMicroelectronics/cmsis_device_wb.git", branch=f"v{device_version}",
+                  shallow=True)
+
+    def package(self):
+        self.copy("*.h", src="Include", dst="include", keep_path=False)
+        # self.copy("stm32l476xx.h", src="Include", dst="include", keep_path=False)
+        # self.copy("system_stm32l4xx.h", src="Include", dst="include", keep_path=False)
+        # self.copy("*.a", src="lib", dst="lib", keep_path=False)
+
+    def package_info(self):
+        if self.settings.arch == "x86_64":
+            self.cpp_info.libs.append(f"{self.name}")
+        self.cpp_info.defines.append("STM32WB55xx")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
