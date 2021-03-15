75056211ac3cf85a6308b908996c26beceaf425c
Michel Meyer <meyer_michel@outlook.com>
2021-03-15 21:53:22 +0100

    refactor build-docker

--- build-docker.py
+++ build-docker.py
@@ -9,14 +9,21 @@ def get_args():
     parser.add_argument("--gitref", type=str, required=False, help="Pass Githubrefs with refs/tags/docker-v*.*.* to push")
     return parser.parse_args()
 
+
+def docker_build_push(target, name, version, push=False):
+    with chdir(os.path.dirname(os.path.realpath(__file__))):
+        run(f'docker build --target {target} -t {name}:{version} .')
+    if push:
+        run(f'docker push {name}:{version}')
+        print(f"docker image {name}:{version} pushed")
+
 if __name__ == '__main__':
     args = get_args()
     ref = str(args.gitref)
     dockerversion="latest"
+    pushImage = False
     if ref.startswith("refs/tags/docker-v"):
         dockerversion=ref.replace("refs/tags/docker-v", "")
-    with chdir(os.path.dirname(os.path.realpath(__file__))):
-        run(f'docker build --target ssh-server -t embedded-hipster-ssh:{dockerversion} .')
-    if dockerversion != "latest":
-        run(f'docker push embedded-hipster-ssh:{dockerversion}')
+        pushImage = True
+    docker_build_push('ssh-server','embedded-hipster-ssh',dockerversion,pushImage)
         
