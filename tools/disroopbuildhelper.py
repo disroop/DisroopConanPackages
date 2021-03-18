e4379655fcb753e9bcac8c0bdb1b625d59be30fc
Stefan Eicher <stefan.eicher@ypsomed.com>
2021-03-18 09:00:24 +0100

    minor disroopbuildhelper.py

--- tools/disroopbuildhelper.py
+++ tools/disroopbuildhelper.py
@@ -6,9 +6,10 @@ def run(cmd, assert_error=False):
     print("*********** Running: %s" % cmd)
     ret = os.system(cmd)
     if ret == 0 and assert_error:
-        raise Exception("Command unexpectedly succedeed: %s" % cmd)
+        raise Exception("Command unexpectedly succeeded: %s" % cmd)
     if ret != 0 and not assert_error:
         raise Exception("Failed command: %s" % cmd)
+
 
 @contextmanager
 def chdir(path):
@@ -18,6 +19,7 @@ def chdir(path):
         yield
     finally:
         os.chdir(current_path)
+
 
 def execute(function):
     try:
