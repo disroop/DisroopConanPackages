d271248f76b1b593dd7d57b4512c6914ca3d51fd
Michel Meyer <meyer_michel@outlook.com>
2021-04-20 22:03:57 +0200

    move assert to stm32_assert.h

--- recipes/stm32_hal_l4/include/stm32l4xx_hal_conf.h
+++ recipes/stm32_hal_l4/include/stm32l4xx_hal_conf.h
@@ -24,7 +24,8 @@
 #ifdef __cplusplus
  extern "C" {
 #endif
-
+#include "inttypes.h"
+#include "stm32_assert.h"
 /* Includes ------------------------------------------------------------------*/
 /**
   * @brief Include module's header file
@@ -186,28 +187,6 @@
  #include "stm32l4xx_hal_hcd.h"
 #endif /* HAL_HCD_MODULE_ENABLED */
 
-/* Exported macro ------------------------------------------------------------*/
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  The assert_param macro is used for function's parameters check.
-  * @param  expr: If expr is false, it calls assert_failed function
-  *         which reports the name of the source file and the source
-  *         line number of the call that failed.
-  *         If expr is true, it returns no value.
-  * @retval None
-  */
-  #define assert_param(expr) ((expr) ? (void)0U : assert_failed((uint8_t *)__FILE__, __LINE__))
-/* Exported functions ------------------------------------------------------- */
-  void assert_failed(uint8_t *file, uint32_t line);
-#else
-  #define assert_param(expr) ((void)0U)
-#endif /* USE_FULL_ASSERT */
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* __STM32L4xx_HAL_CONF_H */
 
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
