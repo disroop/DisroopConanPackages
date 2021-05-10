fdc30f4bb0a0391a7dee7e2cfa395537e446dd22
Michel Meyer <meyer_michel@outlook.com>
2021-05-10 14:02:54 +0200

    add stm32_bsp_Iot_node WIP

---
+++ recipes/stm32_bsp_Iot_node/build.sh
@@ -0,0 +1,3 @@
+conan install -pr:h ../../.profiles/arm-m4-debug -pr:b default -if install .
+conan build -bf install .   
+CMake command: cmake -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE="conan_toolchain.cmake" -DCMAKE_INSTALL_PREFIX="/home/michel/projects/DisroopConanPackages/recipes/stm32_bsp_Iot_node/install/package" "/home/michel/projects/DisroopConanPackages/recipes/stm32_bsp_Iot_node"
