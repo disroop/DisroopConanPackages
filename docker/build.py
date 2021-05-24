da5e796bad616bf06f7171d2f3ec18a358550eb0
Stefan Eicher <stefan.eicher@gmail.com>
2021-05-24 23:58:26 +0200

    cleanup

--- docker/build.py
+++ docker/build.py
@@ -13,25 +13,25 @@ def get_args():
 
 
 def docker_build_push(target, name, version, push=False):
-    dockerhubrepo = "disroop"
+    dockerhub_repo = "disroop"
     client = docker.from_env()
     current_path = os.getcwd()
-    client.images.build(path=current_path, target=target, tag=f'{dockerhubrepo}/{name}:{version}')
+    client.images.build(path=current_path, target=target, tag=f'{dockerhub_repo}/{name}:{version}')
     if push:
-        print(f"docker image {dockerhubrepo}/{name}:{version} push to server:")
-        serveroutput = client.images.push(f"{dockerhubrepo}/{name}", tag=f'{version}')
-        print(f'Server output: {serveroutput}')
+        print(f"docker image {dockerhub_repo}/{name}:{version} push to server:")
+        server_output = client.images.push(f"{dockerhub_repo}/{name}", tag=f'{version}')
+        print(f'Server output: {server_output}')
 
 
 if __name__ == '__main__':
     args = get_args()
     ref = str(args.gitref)
-    dockerversion = "latest"
+    docker_version = "latest"
     pushImage = False
     if ref.startswith("refs/tags/docker-v"):
-        dockerversion = ref.replace("refs/tags/docker-v", "")
+        docker_version = ref.replace("refs/tags/docker-v", "")
         pushImage = True
-    docker_build_push('build-system', 'embedded-hipster', dockerversion, pushImage)
-    docker_build_push('dev', 'embedded-hipster-dev', dockerversion, pushImage)
-    docker_build_push('ssh-server', 'embedded-hipster-ssh', dockerversion, pushImage)
-    docker_build_push('sonar', 'embedded-hipster-sonar', dockerversion, pushImage)
+    docker_build_push('build-system', 'embedded-hipster', docker_version, pushImage)
+    docker_build_push('dev', 'embedded-hipster-dev', docker_version, pushImage)
+    docker_build_push('ssh-server', 'embedded-hipster-ssh', docker_version, pushImage)
+    docker_build_push('sonar', 'embedded-hipster-sonar', docker_version, pushImage)
